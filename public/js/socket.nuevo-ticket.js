36a861ee30ab7827b107a7d78bfbb457c92e9074
Daniel <dicodin@outlook.com>
2020-12-09 19:32:38 -0600

    Primer commit -Produccion

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,21 @@
+// Comando para establecer la conexión
+var socket = io();
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Se ha perdido la conexión con el servidor');
+});
+
+socket.on('estadoActual', function(resp) {
+    label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+    });
+});
